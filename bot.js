1aebd2ac824f1b287e846aa887d5fff66925b5b2
botanimestrue <55363438+botanimestrue@users.noreply.github.com>
2019-09-15 18:32:35 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,81 @@
+﻿const Discord = require("discord.js"); //baixar a lib
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
+
+
+client.on("ready", () => {
+  console.log(`📢Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores!`); 
+  client.user.setPresence({ game: { name: '🎉Sendo programado pelo BL00DKNIGHT :D🎉 e também estou em ${client.channels.size} canais, em ${client.guilds.size} servidores e com ${client.users.size} usuários!', type: 1, url: 'https://www.twitch.tv/bl00dkauan'} });
+    //0 = Jogando
+    //  1 = Transmitindo
+    //  2 = Ouvindo
+    //  3 = Assistindo
+});
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+  
+  // coamdno ping
+  if(comando === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`:ping_pong:Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+  }
+  //comando falar
+  if(comando === "say") { 
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});  
+    message.channel.send(sayMessage);
+  }
+//comando apagar
+  if(comando === "apagar") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Por favor, forneça um número entre 2 e 100 para o número de mensagens a serem excluídas");
+
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`:x:Não foi possível deletar mensagens devido a: ${error}`));
+  }
+  // comando chutar 
+  if(comando === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Desculpe, você não tem permissão para usar isto!");
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Por favor mencione um membro válido deste servidor");
+    if(!member.kickable) 
+      return message.reply("Eu não posso expulsar este usuário! Eles pode ter um cargo mais alto ou eu não tenho permissões de expulsar?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Nenhuma razão fornecida";
+
+    await member.kick(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui expulsar o membro devido o: ${error}`));
+    message.reply(`${member.user.tag} foi kickado por ${message.author.tag} Motivo: ${reason}`);
+
+  }
+  // comando ban
+  if(comando === "ban") {
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Desculpe, você não tem permissão para usar isto!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Por favor mencione um membro válido deste servidor");
+    if(!member.bannable) 
+      return message.reply(":x:Eu não posso banir este usuário! Eles pode ter um cargo mais alto doque o meu!");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Nenhuma razão fornecida";
+    await member.ban(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui banir o membro devido o : ${error}`));
+    message.reply(`${member.user.tag} foi banido por ${message.author.tag} Motivo: ${reason}`);
+  }
+
+});
+
+client.login(config.token); 
